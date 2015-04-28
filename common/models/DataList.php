e84952d3e075b98c851ea916e1c7146ed8b6cc70
petun <petun911@gmail.com>
2015-04-29 00:46:58 +0300

    Добавил метод для отдачи списка

--- common/models/DataList.php
+++ common/models/DataList.php
@@ -3,13 +3,21 @@
 namespace common\models;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "data_list".
  */
 class DataList extends \common\models\base\DataList
 {
-	public function test() {
-
+	/**
+	 * Возвращает все элементы списка определенного типа
+	 * DataList::getList('typeList2')
+	 * @param $typeName
+	 *
+	 * @return array
+	 */
+	public static function getList($typeName) {
+		return ArrayHelper::map(self::find()->where(['type' => $typeName])->orderBy(['position' => SORT_ASC])->all(), 'id', 'name');
 	}
 }
