2a6328f7f5dfc29403648756bc2ac4a39edb1ea2
Ignatenkov Nikita <ignatenkov.n@gmail.com>
2016-06-07 13:36:06 +0300

    Add csv import 2 class

---
+++ common/components/CSV/CSVImporter.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @copyright Copyright Victor Demin, 2015
+ * @license https://github.com/ruskid/yii2-csv-importer/LICENSE
+ * @link https://github.com/ruskid/yii2-csv-importer#README
+ */
+
+namespace common\components\CSV;
+
+
+/**
+ * Little CSV import helper for Yii2
+ * @author Victor Demin <demin@trabeja.com>
+ */
+class CSVImporter {
+
+    private $_data;
+
+    /**
+     * @param CSVReader $reader
+     */
+    public function setData(CSVReader $reader) {
+        $this->_data = $reader->readFile();
+    }
+
+    /**
+     * Will get CSV data
+     * @return array
+     */
+    public function getData() {
+        return $this->_data;
+    }
+
+    /**
+     * Will import csv file using strategy.
+     * @param ImportInterface $strategy
+     */
+    public function import(ImportInterface $strategy) {
+        return $strategy->import($this->_data);
+    }
+
+}
